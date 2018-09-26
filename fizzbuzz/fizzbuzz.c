bf9fb9acd1ca565daa188da1cb78b8277f80914c
Stephanie Bowles <sbowl001@fiu.edu>
2018-09-26 11:58:27 +0800

    fizzbuzz working

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,25 @@
 */
 int fizzbuzz(int n)
 {
-
+  int counter = 0;
+  for(int i = 0; i <= n ; i++)
+  {
+    if ( i % 3 == 0 && i % 5 == 0){
+       printf("%d\n", i);
+        printf("Fizzbuzz\n");
+      }
+      else if(i % 5 == 0){
+       printf("%d\n", i);
+        printf("Fuzz\n");
+      }
+      else if (i % 3 == 0){
+       printf("%d\n", i);
+        printf("Fizz\n");
+      }
+     else {
+         counter++;
+    } 
+    } return counter;
 }
 
 #ifndef TESTING
